097e93e9676459c3dfdf6711d0a06602cdb64697
Abdurrahman Ayyıldız <ayyildizabdurrahman@gmail.com>
2018-03-18 23:52:17 +0300

    Update primeChecker.c

--- primeChecker.c
+++ primeChecker.c
@@ -15,32 +15,51 @@ int prime(int num)
 	if(counter==2)
 	{
 		printf("%d ", num);
+		return 1;
 	}
-
-	return 0;
+	
+	else{
+		return 0;
+	}
 }
 
 int main()
 {
+	int flag = 0;
 	int num1, num2, num3, num4, num5, 
 	    num6, num7, num8, num9, num10;
+	
+	while(flag != -1)
+	{
+		printf("\nEnter 10 numbers between 0 and 1000:\n");
+		scanf("%d %d %d %d %d %d %d %d %d %d", &num1, &num2, &num3, &num4, &num5, 
+	        	                               &num6, &num7, &num8, &num9, &num10);
 
-	printf("Enter 10 numbers between 0 and 1000:\n");
-	scanf("%d %d %d %d %d %d %d %d %d %d", &num1, &num2, &num3, &num4, &num5, 
-	                                       &num6, &num7, &num8, &num9, &num10);
+		printf("\nThe prime numbers: ");
 
-	printf("The prime numbers: ");
+		num1 = prime(num1);
+		num2 = prime(num2);
+		num3 = prime(num3);
+		num4 = prime(num4);
+		num5 = prime(num5);
+		num6 = prime(num6);
+		num7 = prime(num7);
+		num8 = prime(num8);
+		num9 = prime(num9);
+		num10 = prime(num10);
 
-	prime(num1);
-	prime(num2);
-	prime(num3);
-	prime(num4);
-	prime(num5);
-	prime(num6);
-	prime(num7);
-	prime(num8);
-	prime(num9);
-	prime(num10);
 
+		if( num1==0 && num2==0 && num3==0 && num4==0 && num5==0 &&
+			num6==0 && num7==0 && num8==0 && num9==0 && num10==0 )
+		{
+			printf("\nThere is not a prime number!\n");
+		}
+
+		else
+		{
+			printf("\n");
+			flag = -1;
+		}
+	}
 	return 0;
 }
